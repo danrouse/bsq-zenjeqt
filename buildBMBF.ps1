f4769cf4729a61640b5016d5e308e724028e3e50
Dan Rouse <dan@rouse.house>
2021-02-12 16:18:56 -0500

    setup for initial release

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libzenjeqt.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_10.so", "./bmbfmod.json" -DestinationPath "./zenjeqt_v0.1.0.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libzenjeqt.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json" -DestinationPath "./zenjeqt_v0.1.0.zip" -Update
 }
---
+++ buildGithub.ps1
@@ -0,0 +1,5 @@
+mkdir github_release
+& .\buildBMBF.ps1
+mv zenjeqt_v0.1.0.zip github_release
+cp libs\arm64-v8a\libzenjeqt.so github_release
+cp obj\local\arm64-v8a\libzenjeqt.so github_release\libzenjeqt_debug.so
