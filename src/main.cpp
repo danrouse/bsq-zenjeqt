aa6565ab2ee5008d32639bbff3fdd557846bad7f
Dan Rouse <dan@rouse.house>
2021-02-20 12:49:34 -0500

    modinfo ig

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,10 @@ Logger& getLogger() {
     return *logger;
 }
 
-extern "C" void setup(ModInfo& info) {}
+extern "C" void setup(ModInfo& info) {
+    info.id = ID;
+    info.version = VERSION;
+}
 
 extern "C" void load() {
     il2cpp_functions::Init();
