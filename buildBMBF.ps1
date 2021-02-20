cb84dc1df5268dedf9cd8f33ffe87202e516a934
Dan Rouse <dan@rouse.house>
2021-02-20 14:13:48 -0500

    0.1.2

--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -2,5 +2,5 @@
 & $PSScriptRoot/build.ps1
 
 if ($?) {
-    Compress-Archive -Path "./libs/arm64-v8a/libzenjeqt.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json" -DestinationPath "./zenjeqt_v0.1.1.zip" -Update
+    Compress-Archive -Path "./libs/arm64-v8a/libzenjeqt.so", "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so", "./bmbfmod.json" -DestinationPath "./zenjeqt_v0.1.2.zip" -Update
 }
